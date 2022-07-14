f8a81472b15fa0e33b36bbb17e07cab9d545f5c9
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-07-14 17:01:10 +0300

    +add: PlayerController and Camera Controller added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //[SerializeField] private ScoreSO scoreType = null;    // Scriptable Objects eriþir 
+
+    public static GameManager gamemanagerInstance;
+    public bool gameStart;  // Oyun basladimi
+    public bool isFinish;   // Finish alanına girdimi
+    private void Awake()
+    {
+        if (gamemanagerInstance == null)
+        {
+            gamemanagerInstance = this;
+        }
+    }
+    void Start()
+    {
+        gameStart = false;  // oyun yuklendiginde gameStart false olarak baslar
+        isFinish = false; // oyun yuklendiginde isFinish false olarak baslar
+        StartTextReset();   // ana ekrandaki text yazıları gunceller
+    }
+    void StartTextReset()
+    {
+        // ana ekrandaki text yazıları gunceller
+        //scoreType.currentCoin = scoreType.minCoin;
+        //UIController.uicontrollerInstance.GamePlayGoldText.text = scoreType.minCoin.ToString();
+    }
+    void Update()
+    {
+        
+    }
+    public void AddCoin()
+    {
+        // Altın Ekler
+        Debug.Log("Coin added");
+        //AudioController.audioControllerInstance.Play("CoinSound");  // Coin ses acar
+        //scoreType.totalCoin+=scoreType.currentCoin++;
+        //UIController.uicontrollerInstance.GamePlayCoinText.text = scoreType.totalCoin.ToString();
+        // Coin ekler ve text gunceller
+    }
+    public void Finish()
+    {
+        isFinish = true;  
+        //UIController.uicontrollerInstance.WinPanelActive();    // LosePanel Açıl 
+        //UIController.uicontrollerInstance.WinPanelText();        // WinPanel Açıl
+    }
+    public void RetyLevel()
+    {
+        // Yeniden başlat
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void GamePausePlay(int value)
+    {
+        Time.timeScale = value;
+        // Oyunu durdur
+    }
+    public void GameExit()
+    {
+        // Oyundan çık
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //[SerializeField] private CharacterSO characterType = null;    // Scriptable Objects eriþir 
+
+    private Rigidbody rb;
+    private Animator anim;
+    [Space]
+    [Header("Player Controller")]
+    [SerializeField] private float _speed = 10f;    // Karaker hizi
+    [SerializeField] private float _flySpeed = 100f;   // Karakter zıplama gucu 
+    [SerializeField] private bool _isRunning;   // Zıplama aktif mi
+    [SerializeField] private bool _isFly;   // Zıplama aktif mi
+    [SerializeField] private float _flyTime;  // Kalkan süresi
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _isFly = false;
+        //flyTime=characterType.flyTime;
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Ekrana tıklandığında            
+            TapToStart();
+        }
+        if (Input.GetMouseButton(0) && GameManager.gamemanagerInstance.gameStart)
+        {
+            // Ekrana basılı tutulduğunda
+            _isRunning = true; // Running aktif olur
+            anim.SetBool("Running", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Ekrana dokunma bırakıldığında
+            _isRunning = false; // Running pasif olur 
+            anim.SetBool("Running", false);
+        }         
+    }
+    private void FixedUpdate()
+    {
+        if (GameManager.gamemanagerInstance.gameStart && !GameManager.gamemanagerInstance.isFinish && _isRunning)
+        {
+            Move();
+            // eger gameStart , isRunning true ve isFinish false ise (oyun baslamıs) hareker et
+        }
+        else
+        {
+            rb.velocity = Vector3.zero; // sabit kal
+        }
+        if (GameManager.gamemanagerInstance.gameStart && _isFly && GameManager.gamemanagerInstance.isFinish)
+        {
+            Fly();
+            //eger gameStart true, _isFly true ve isFinish true ise (oyun baslamıs) uc
+        }
+    }
+    public void TapToStart()
+    {
+        // oyunu baslatmak icin ekrana tıklanır     
+        GameManager.gamemanagerInstance.gameStart = true;   // gameStart aktif olur
+        UIController.uicontrollerInstance.GamePlayActive(); // GamePlay alanındaki textler akif olur
+    }
+    void Move()
+    {
+        transform.Translate(0, 0, _speed * Time.fixedDeltaTime); // Karakter speed degeri hizinda ileri hareket eder
+    }
+    void Fly()
+    {
+        rb.AddForce(transform.up * _flySpeed * Time.fixedDeltaTime, ForceMode.Impulse);    // Yukari yonde zıplma gucu uygular
+        rb.AddForce(transform.forward * _speed * Time.fixedDeltaTime, ForceMode.Impulse);    // Yukari yonde zıplma gucu uygular
+        //flyEffect.Play(); // Ekrana basmayınca jetpack efekti calisir  
+    }
+}
