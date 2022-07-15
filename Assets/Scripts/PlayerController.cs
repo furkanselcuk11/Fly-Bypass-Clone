edab7c7c88b8e8d5c7d564cc9c932f448f9fa8b8
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-07-15 14:42:06 +0300

    +add: Flying added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,52 +12,62 @@ public class PlayerController : MonoBehaviour
     [Header("Player Controller")]
     [SerializeField] private float _speed = 10f;    // Karaker hizi
     [SerializeField] private float _flySpeed = 100f;   // Karakter zıplama gucu 
-    [SerializeField] private bool _isRunning;   // Zıplama aktif mi
+    [SerializeField] private bool _isMove;   // Zıplama aktif mi
+    [SerializeField] private bool _isGround;   // Zıplama aktif mi
     [SerializeField] private bool _isFly;   // Zıplama aktif mi
     [SerializeField] private float _flyTime;  // Kalkan süresi
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         _isFly = false;
+        _isGround = true;
         //flyTime=characterType.flyTime;
         anim = GetComponent<Animator>();
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!GameManager.gamemanagerInstance.isFinish)
         {
-            // Ekrana tıklandığında            
-            TapToStart();
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Ekrana tıklandığında            
+                TapToStart();
+            }
+            if (Input.GetMouseButton(0) && GameManager.gamemanagerInstance.gameStart && _isGround)
+            {
+                // Ekrana basılı tutulduğunda
+                _isMove = true; // Running aktif olur
+                anim.SetBool("Running", true);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                // Ekrana dokunma bırakıldığında
+                _isMove = false; // Running pasif olur 
+                anim.SetBool("Running", false);
+            }
         }
-        if (Input.GetMouseButton(0) && GameManager.gamemanagerInstance.gameStart)
-        {
-            // Ekrana basılı tutulduğunda
-            _isRunning = true; // Running aktif olur
-            anim.SetBool("Running", true);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            // Ekrana dokunma bırakıldığında
-            _isRunning = false; // Running pasif olur 
-            anim.SetBool("Running", false);
-        }         
+                
     }
     private void FixedUpdate()
     {
-        if (GameManager.gamemanagerInstance.gameStart && !GameManager.gamemanagerInstance.isFinish && _isRunning)
+        if (GameManager.gamemanagerInstance.gameStart)
         {
-            Move();
-            // eger gameStart , isRunning true ve isFinish false ise (oyun baslamıs) hareker et
+            if (!GameManager.gamemanagerInstance.isFinish && _isMove)
+            {
+                Move();
+                // eger gameStart , isRunning true ve isFinish false ise (oyun baslamıs) hareker et
+            }
+            else if (!GameManager.gamemanagerInstance.isFinish && !_isMove)
+            {
+                rb.velocity = Vector3.zero; // sabit kal
+            }
+            if (GameManager.gamemanagerInstance.isFinish && _isFly)
+            {
+                Fly();
+                //eger gameStart true, _isFly true ve isFinish true ise (oyun baslamıs) uc
+            }
         }
-        else
-        {
-            rb.velocity = Vector3.zero; // sabit kal
-        }
-        if (GameManager.gamemanagerInstance.gameStart && _isFly && GameManager.gamemanagerInstance.isFinish)
-        {
-            Fly();
-            //eger gameStart true, _isFly true ve isFinish true ise (oyun baslamıs) uc
-        }
+        
     }
     public void TapToStart()
     {
@@ -71,8 +81,38 @@ public class PlayerController : MonoBehaviour
     }
     void Fly()
     {
-        rb.AddForce(transform.up * _flySpeed * Time.fixedDeltaTime, ForceMode.Impulse);    // Yukari yonde zıplma gucu uygular
-        rb.AddForce(transform.forward * _speed * Time.fixedDeltaTime, ForceMode.Impulse);    // Yukari yonde zıplma gucu uygular
-        //flyEffect.Play(); // Ekrana basmayınca jetpack efekti calisir  
+        //flyEffect.Play(); // Ucarken Flying efekti calisir  
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 3f, transform.position.z), 1f*Time.fixedDeltaTime);
+        transform.Translate(0, 0, _flySpeed * Time.fixedDeltaTime); // Karakter speed degeri hizinda ileri hareket eder
+        _flyTime -= Time.deltaTime;
+        if (_flyTime <= 0)
+        {
+            rb.useGravity = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            // Finish cizgisine  girmis ise
+            GameManager.gamemanagerInstance.isFinish = true;
+            _isGround = false;
+            _isFly = true;
+            _isMove = false;
+            rb.useGravity = false;
+            anim.SetBool("Flying", true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Ground");
+            GameManager.gamemanagerInstance.isFinish = false;
+            _isGround = true;
+            _isFly = false;
+            _flyTime = 3f;
+            anim.SetBool("Flying", false);
+        }
     }
 }
